a3d9b11e8d0b938ef8683766b1f5b8b956a191e9
Isaac Santos <isaac.emanuel@live.com>
2025-02-12 00:43:33 -0600

    +Flutter project (quiz hardcoded logic)

---
+++ lib/data/color_palete.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const blackPurple = Color.fromARGB(255, 21, 0, 22);
+const deepPurple = Color.fromARGB(255, 41, 16, 74);
+const purple = Color.fromARGB(255, 82, 44, 93);
+const white = Color.fromARGB(255, 255, 227, 216);
+const royalPurple = Color.fromARGB(255, 118, 0, 124);
+const violet = Color.fromARGB(255, 151, 71, 255);
