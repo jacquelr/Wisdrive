39253c27ed110de416f5450c140b300a303540d0
Isaac Santos <isaac.emanuel@live.com>
2025-02-16 18:19:15 -0600

    perf(login_screen)reducing code calling clases of login buttons

--- lib/navigation/screens/login_screen.dart
+++ lib/navigation/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:quiz_app/data/app_theme.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/widgets/login_buttons.dart';
 
 const logo = '../assets/images/logo.png';
 
@@ -28,37 +29,9 @@ class LoginScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: 100),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      foregroundColor: Colors.white),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Text(
-                      'iniciar sesion',
-                      style: GoogleFonts.play(color: Colors.white, fontSize: 30),
-                    ),
-                  ),
-                ),
-              ),
+              const LoginButton(),
               const SizedBox(height: 15),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Text(
-                      'registrarse',
-                      style: GoogleFonts.play(color: purple, fontSize: 30),
-                    ),
-                  ),
-                ),
-              ),
+              const SignupButton(),
             ],
           ),
         ),
