4e47b69502b9f1576da12eff01036a402924d520
Isaac Santos <isaac.emanuel@live.com>
2025-03-06 22:59:00 -0600

    feat(constraints):set and get images routes and helper functions

---
+++ lib/constraints/images_routes.dart
@@ -0,0 +1,7 @@
+class RImages {
+
+  static const String onboarding_1 = "assets/images/onboarding-1.png";
+  static const String onboarding_2 = "assets/images/onboarding-2.png";
+  static const String onboarding_3 = "assets/images/onboarding-3.png";
+
+}
