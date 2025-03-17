ce8213572ec308e90feeb92abb41c825e558df9b
repoCodigo-so <gitjuanjo@gmail.com>
2025-03-17 12:51:15 -0600

    Feat: add chatpdf feature and dot env package

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,24 @@ import 'package:quiz_app/data/app_theme.dart';
 import 'package:quiz_app/navigation/screens/splash_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'generated/l10n.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const logo = '../assets/images/logo.png';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  try {
+    await dotenv.load(fileName: ".env"); // Load environment variables
+  } catch (e) {
+    throw Exception('Error loading .env file: $e'); // Print error if any
+  }
   // Inicializar Supabase
   await Supabase.initialize(
-    url: 'https://vtyjpcahodmndprwngxx.supabase.co', // Reemplaza con tu URL de Supabase
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0eWpwY2Fob2RtbmRwcnduZ3h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyNzgyNjIsImV4cCI6MjA1NTg1NDI2Mn0.sqWU9JAlMmUbk7z1PpGeC2Xow9cg6JjU9-eb0NjqDvY', // Reemplaza con tu API Key pública
+    url:
+        'https://vtyjpcahodmndprwngxx.supabase.co', // Reemplaza con tu URL de Supabase
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0eWpwY2Fob2RtbmRwcnduZ3h4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyNzgyNjIsImV4cCI6MjA1NTg1NDI2Mn0.sqWU9JAlMmUbk7z1PpGeC2Xow9cg6JjU9-eb0NjqDvY', // Reemplaza con tu API Key pública
   );
 
   Get.put(ThemeController());
