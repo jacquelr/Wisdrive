fbac68e9bff80d44b5f53d82ce69e8e1f2fbc37a
Isaac Santos <isaac.emanuel@live.com>
2025-04-24 23:18:59 -0600

    building latest version of apk on cellphone

--- lib/widgets/home/basic_mecanic.dart
+++ lib/widgets/home/basic_mecanic.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:get/get.dart';
+import 'package:wisdrive/constraints/app_theme.dart';
 import 'package:wisdrive/controllers/theme_controller.dart';
-import 'package:wisdrive/data/app_theme.dart';
 import 'package:wisdrive/generated/l10n.dart';
 import 'package:wisdrive/widgets/home/video_mecanic.dart';
 
